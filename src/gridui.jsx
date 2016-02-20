d617d12568c23ca34c69b66cf79605c37356d37d
Stephen Clavering <stephen@clavering.me.uk>
2016-02-20 16:43:16 +0000

    improve performance by implementing .shouldComponentUpdate() for more components
    Prior to this rotations in hex games were very noticeably laggy in Firefox, and this mostly fixes that.
    
    The major change is not rerendering GameWalls and individual tile backgrounds.
    
    Also, this replaces several basically-equivalent .shouldComponentUpdate() implementations with a single <PureWrapper/> component.

--- src/gridui.jsx
+++ src/gridui.jsx
@@ -61,26 +61,36 @@ const GameUI = React.createClass({
             <svg viewBox={ "0 0 " + params.view_width + " " + params.view_height } preserveAspectRatio="xMidYMid meet" width="100%" height="100%">
                 <GameBackground component={ params.bg_component } grid={ grid } on_tile_click={ on_tile_click } locked_set={ this.state.grid_state.locked_set }/>
                 <GameTiles component={ params.tile_component } grid={ grid } grid_state={ this.state.grid_state }/>
-                <GameWalls component={ params.walls_component } grid={ grid }/>
+                <PureWrapper component={ GameWalls } walls_component={ params.walls_component } grid={ grid }/>
             </svg>
         </div>;
     },
 });
 
 function GameBackground(props) {
-    const BackgroundComponent = props.component;
-    return <g>{ props.grid.tiles.map((tile, ix) => <BackgroundComponent key={ ix } is_locked={ tile.id in props.locked_set } tile={ tile } onClick={ ev => props.on_tile_click(ev, tile) }/>) }</g>;
+    return <g>{ props.grid.tiles.map((tile, ix) => <PureWrapper component={ props.component } key={ ix } is_locked={ tile.id in props.locked_set } tile={ tile } onClick={ ev => props.on_tile_click(ev, tile) }/>) }</g>;
 };
 
 function GameTiles(props) {
-    const TileComponent = props.component;
-    return <g>{ props.grid.tiles.map((tile, ix) => <TileComponent key={ ix } tile={ tile } orientation={ props.grid_state.orientations[tile.id] } is_powered={ !!props.grid_state.powered_set[tile.id] }/>) }</g>;
+    return <g>{ props.grid.tiles.map((tile, ix) => <PureWrapper component={ props.component } key={ ix } tile={ tile } orientation={ props.grid_state.orientations[tile.id] } is_powered={ !!props.grid_state.powered_set[tile.id] }/>) }</g>;
 };
 
 function GameWalls(props) {
-    const WallsComponent = props.component;
+    const WallsComponent = props.walls_component;
     return <g>{ props.grid.tiles.map((tile, ix) => <WallsComponent key={ ix } tile={ tile }/>) }</g>;
 };
+
+
+const PureWrapper = React.createClass({
+    shouldComponentUpdate: function(next_props, _next_state) {
+        // This assumes the prop names aren't changing, but that's almost certainly correct.
+        for(let k in next_props) if(this.props[k] !== next_props[k]) return true;
+        return false;
+    },
+    render() {
+        return React.createElement(this.props.component, this.props);
+    },
+});
 
 
 function SquareBackground(props) {
@@ -102,28 +112,18 @@ function SquareWall(props) {
     return <line x1={ props.x1 * sqr_size } x2={ props.x2 * sqr_size } y1={ props.y1 * sqr_size } y2={ props.y2 * sqr_size } style={{ stroke: wall_colour, strokeWidth: 5, strokeLinecap: "round" }}/>;
 };
 
-const SquareTile = React.createClass({
-    shouldComponentUpdate: function(next_props, _next_state) {
-        for(let k in next_props) if(next_props[k] !== this.props[k]) return true;
-        return false;
-    },
-    render: function() {
-        const { tile, orientation, is_powered } = this.props;
+function SquareTile(props) {
+        const { tile, orientation, is_powered } = props;
         // Note: .style.stroke is inherited by the descendant line segments.
         return <g transform={ "translate(" + (tile.x * sqr_size + sqr_half) + "," + (tile.y * sqr_size + sqr_half) + ")" } style={{ stroke: is_powered ? line_powered_colour : line_colour }}>
             <g transform={ "rotate(" + (orientation * 90) + ")" }>
-                <SquareTileInner tile={ tile }/>
+                <PureWrapper component={ SquareTileInner } tile={ tile }/>
             </g>
         </g>;
-    },
-});
+};
 
-const SquareTileInner = React.createClass({
-    shouldComponentUpdate: function(next_props, _next_state) {
-        return this.props.tile !== next_props.tile;
-    },
-    render() {
-        const tile = this.props.tile;
+function SquareTileInner(props) {
+        const tile = props.tile;
         return <g style={{ pointerEvents: "none" }}>
             { tile.links[0] ? <SquareLine angle={ 0 }/> : null }
             { tile.links[1] ? <SquareLine angle={ 90 }/> : null }
@@ -132,8 +132,7 @@ const SquareTileInner = React.createClass({
             { tile.is_source ? <rect x={ -sqr_source_half_size } y={ -sqr_source_half_size } width={ 2 * sqr_source_half_size } height={ 2 * sqr_source_half_size } stroke={ line_colour }/> : null }
             { !tile.is_source && tile.is_leaf_node ? <circle r={ sqr_node_radius } stoke={ line_colour } fill={ node_colour }/> : null }
         </g>;
-    },
-});
+};
 
 function SquareLine(props) {
     return <line y2={ -sqr_half } transform={ "rotate(" + props.angle + ")" } style={{ strokeWidth: 5, strokeLinecap: "round", fill: "none" }}/>;
@@ -161,28 +160,18 @@ function HexWall(props) {
     return <line x1={ -hex_half_width } x2={ -hex_overhang } y2={ -hex_half_height } transform={ hex_center_translate(props.tile) + " rotate(" + props.rotate + ")" } style={{ stroke: wall_colour, strokeWidth: 5, strokeLinecap: "round" }}/>;
 };
 
-const HexTile = React.createClass({
-    shouldComponentUpdate: function(next_props, _next_state) {
-        for(let k in next_props) if(next_props[k] !== this.props[k]) return true;
-        return false;
-    },
-    render: function() {
-        const { tile, orientation, is_powered } = this.props;
+function HexTile(props) {
+        const { tile, orientation, is_powered } = props;
         // Note: .style.stroke is inherited by the descendant line segments.
         return <g transform={ hex_center_translate(tile) } style={{ stroke: is_powered ? line_powered_colour : line_colour }}>
             <g transform={ "rotate(" + (orientation * 60) + ")" }>
-                <HexTileInner tile={ tile }/>
+                <PureWrapper component={ HexTileInner } tile={ tile }/>
             </g>
         </g>;
-    },
-});
+};
 
-const HexTileInner = React.createClass({
-    shouldComponentUpdate: function(next_props, _next_state) {
-        return this.props.tile !== next_props.tile;
-    },
-    render() {
-        const tile = this.props.tile;
+function HexTileInner(props) {
+        const tile = props.tile;
         return <g style={{ pointerEvents: "none" }}>
             { tile.links[0] ? <HexLine angle={ -60 }/> : null }
             { tile.links[1] ? <HexLine angle={ 0 }/> : null }
@@ -193,8 +182,7 @@ const HexTileInner = React.createClass({
             { tile.is_source ? <path d={ hex_path } transform="scale(0.5)" stroke={ line_colour }/> : null }
             { !tile.is_source && tile.is_leaf_node ? <circle r={ hex_node_radius } stoke={ line_colour } fill={ node_colour }/> : null }
         </g>;
-    },
-});
+};
 
 function HexLine(props) {
     return <line y2={ -hex_half_height } transform={ "rotate(" + props.angle + ")" } style={{ strokeWidth: 5, strokeLinecap: "round", fill: "none" }}/>;
